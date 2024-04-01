de1b360f63ecec6523671c94e7c03082e215d67c
EdmilsonSoares <edmilsonsoares85@hotmail.com>
2024-04-01 05:17:54 -0300

    Pacote herança finalizado

---
+++ AulaPOO/src/Herança/ClasseFilha3.java
@@ -0,0 +1,10 @@
+package Herança;
+
+public class ClasseFilha3 extends ClasseFilha1{
+	
+	ClasseFilha3(){
+		super();
+	}
+	
+
+}
--- AulaPOO/src/Herança/ConceitoHeranca.java
+++ AulaPOO/src/Herança/ConceitoHeranca.java
@@ -6,7 +6,16 @@ public class ConceitoHeranca {
 		ClasseFilha2 a = new ClasseFilha2();
 		
 		a.setAtributo1("Valor do atributo 1");
-
+		a.setAtributo2("Valor do atributo 2");
+		System.out.println(a.getAtributo1());
+		System.out.println(a.getAtributo2());
+		
+		ClasseFilha3 b = new ClasseFilha3();
+		b.setAtributo1("Atributo 1 da classeFilha3");
+		b.setAtributo2("Atributo 2 da classeFilha3");
+		System.out.println();
+		System.out.println(b.getAtributo1());
+		System.out.println(b.getAtributo2());
 	}
 
 }
