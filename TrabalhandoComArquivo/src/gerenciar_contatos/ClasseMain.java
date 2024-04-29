5cdff15b392eeb147a078fd77bbd849d6d1fefef
EdmilsonSoares <edmilsonsoares85@hotmail.com>
2024-04-29 15:55:14 -0300

    Comentário

--- TrabalhandoComArquivo/src/gerenciar_contatos/ClasseMain.java
+++ TrabalhandoComArquivo/src/gerenciar_contatos/ClasseMain.java
@@ -8,6 +8,7 @@ public class ClasseMain {
 		JFrame janela = new Janela();
 		String t1, t2, t3;
 		
+		//tanto a janela como os JOptionPane executam ao mesmo tempo
 		t1 = JOptionPane.showInputDialog(null, "Digite alguma coisa");
 		t2 = JOptionPane.showInputDialog(null, "Digite alguma coisa");
 		t3 = JOptionPane.showInputDialog(null, "Digite alguma coisa");
